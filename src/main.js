9ddab7d3aba35605a269701113ff04aac30e31e0
XRene <1017653702@qq.com>
2017-03-07 19:43:15 +0800

    update all files

--- src/main.js
+++ src/main.js
@@ -3,33 +3,10 @@ import App from './App';
 import router from './route';
 import axios from 'axios';
 import './less/index';
-const VueI18n = require('vue-i18n');
 
 
 Vue.config.debug = true;
 Vue.prototype.http = axios;
-
-var locales = {
-  en: {
-    message: {
-      hello: 'hello world'
-    }
-  },
-  ja: {
-    message: {
-      hello: 'こんにちは、世界'
-    }
-  }
-}
-
-Vue.use(VueI18n, {
-  lang: 'ja',
-  locales: locales
-});
-
-Vue.config.lang = 'ja';
-Object.keys(locales).forEach(lang => Vue.locale(lang, locales[lang]))
-
 
 const app = new Vue({
   router,
